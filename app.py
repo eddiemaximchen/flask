77afcadf8790e089832bff18ef0c709a44684f0c
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-04-19 10:38:48 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,url_for
+
+
+app = Flask(__name__)
+
+user={
+    'username':'Grey li',
+    'bio':'A boy who loves movies and music.'
+}
+movies=[
+    {'name':'my neighbor','year':'1998'},
+    {'name':'Forrest Gump','year':"1993"},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html',user=user,movies=movies)
+if __name__ == '__main__':
+
+    app.run(debug=True)
+
+
+
+
