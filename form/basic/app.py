434093b48576565c3923728cd260d439fc58574d
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-04-21 17:39:49 +0800

    Update app.py

--- form/basic/app.py
+++ form/basic/app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
 
-@app.route('/basic')
+@app.route('/basic',methods=['GET','POST'])
 def basic():
     form=LoginForm()
     return render_template('basic.html',form=form)
