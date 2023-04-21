88abb5b1e620098942aeb5597cea76393c876600
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-04-21 13:24:44 +0800

    Add files via upload

---
+++ form/basic/app.py
@@ -0,0 +1,15 @@
+from forms import LoginForm
+from flask import Flask,render_template
+import os
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'secret string')
+
+@app.route('/basic')
+def basic():
+    form=LoginForm()
+    return render_template('basic.html',form=form)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
