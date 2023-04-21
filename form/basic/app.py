94803a01b042c6479bbff6d063ff3481494154ff
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-04-21 17:47:52 +0800

    Update app.py

--- form/basic/app.py
+++ form/basic/app.py
@@ -1,14 +1,21 @@
 from forms import LoginForm
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import os
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
+username={
+        'username':'eddie'
+}
 
 @app.route('/basic',methods=['GET','POST'])
 def basic():
     form=LoginForm()
-    return render_template('basic.html',form=form)
+    if request.method=="POST" and form.validate():
+        username = form.username.data
+        return 'ok'
+    return render_template('basic.html',form=form,username=username)
+
 
 if __name__ == '__main__':
 
